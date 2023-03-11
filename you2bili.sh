d036f80cd8e346f9ff1d0dbf52d1c99a05c295bd
LucasLee99 <126792973+LucasLee99@users.noreply.github.com>
2023-03-11 12:17:12 +0800

    添加一键只需一键即可扫码登录
    自动监控自己喜欢的主播自动录制保存发到B站

---
+++ you2bili.sh
@@ -0,0 +1,22 @@
+mkdir biliupdir \
+&& cd biliupdir \
+&& pip install biliup \
+&& wget -c https://github.com/ForgQi/biliup-rs/releases/download/v0.1.15/biliupR-v0.1.15-x86_64-linux.tar.xz \
+&& tar -xvf ./biliupR-v0.1.15-x86_64-linux.tar.xz \
+&& mv biliupR-v0.1.15-x86_64-linux/ biliupR
+
+# 修改 config.toml 文件，添加需要被监控的直播间，自定义添加直播间
+cat >config.toml <<EOF
+# 以下为必填项
+[streamers."Lofi Girl"] # 设置直播间1
+url = ["https://www.youtube.com/watch?v=jfKfPfyJRdk"]
+tags = ["安静学习"]
+
+# 设置直播间2
+[streamers."在线追逐"]
+url = ["https://www.youtube.com/watch?v=iNRIA8HE0DA"]
+tags = ["追逐"]            
+EOF
+
+# 扫码登录 B站登录
+cd biliupR && ./biliup login && biliup start 
